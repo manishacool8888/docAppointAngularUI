cff652d87378fcd521b87a7ea56adef2c445450c
manishacool8888 <manisha.bgs1998@gmail.com>
2020-04-06 19:30:58 +0530

    changes for search doctor

--- src/app/patient/search-doctor/search-doctor.component.ts
+++ src/app/patient/search-doctor/search-doctor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DocAppointCommonService } from 'src/app/service/data/doc-appoint-common.service';
 
 @Component({
   selector: 'app-search-doctor',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchDoctorComponent implements OnInit {
 
-  constructor() { }
+  stateList : string[];
+  cityList : string[];
+  localityList : string[];
+
+  selectedState='';
+  selectedCity='';
+  selectedLocality='';
+
+  constructor(private docAppointService : DocAppointCommonService) { }
 
   ngOnInit() {
+    this.getStateList();
+  }
+
+
+  getStateList(){
+    this.docAppointService.retrieveStates().subscribe(
+      response => {
+        this.stateList=response;
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  getcityList(state){
+    this.docAppointService.retrieveCity(state).subscribe(
+      response => {
+        this.cityList=response;
+        console.log("cityList is :"+this.cityList);
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  getLocalityList(city){
+    this.docAppointService.retrieveLocality(city).subscribe(
+      response => {
+        this.localityList=response;
+        console.log("localityList is :"+this.cityList);
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
+  seachDoctor(){
+    
   }
 
 }
